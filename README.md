cd4eb39247433da4b5ecdcf0457d4a23d733800e
EdouardQ <edouard.quilliou@gmail.com>
2022-06-07 10:53:43 +0200

    commit 0.02

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # H3discord
+
+## Requirements
+- discord.py
+- python-dotenv
+
+## Features : 
+- hello
+- pierre / feuille / ciseaux
+- musique
+- photo
+- retard ratp
+- Meven
--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 import discord
+import os
+from dotenv import load_dotenv
 
-client = discord.Client()
+class MyClient(discord.Client):
+    input_channel = 983651129969770516
+    ratp_channel = 983651171094921226
+    music_channel = 983651061044764692
 
-client.run("OTgzNjM3OTI0ODAzNDA3ODkz.GTLifL.NCMEPQE-EnwqjgRFIRrcxgtVCAp2I1sf9qdIf8")
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!hello'):
+            await message.channel.send('Hello {0.author.mention}'.format(message))
+
+load_dotenv()
+DISCORD_TOKEN = os.getenv("discord_token")
+
+client = MyClient()
+client.run(DISCORD_TOKEN)
