44c38db3a1539e0bcfa2d9703b944aec8456a9d4
EdouardQ <edouard.quilliou@gmail.com>
2022-06-10 14:19:14 +0200

    Update shifumi.py

--- shifumi.py
+++ shifumi.py
@@ -9,20 +9,19 @@ async def play(ctx, shifumi_player):
         embed = discord.Embed(title="", description="I don't recognize what did you play", color=discord.Color.red())
         return await ctx.send(embed=embed)
 
-    shifumi_bot = shifumi_list[random.randint(0, 2)]
+    shifumi_player_key = shifumi_list.index(shifumi_player)
 
-    if shifumi_bot == shifumi_player:
+    shifumi_bot_key = random.randint(0, 2)
+    shifumi_bot = shifumi_list[shifumi_bot_key]
+
+    result = shifumi_player_key - shifumi_bot_key
+
+    if result == 0:
         embed = discord.Embed(title="", description="I play '" + shifumi_bot + "'\nIt's a draw",
                               color=discord.Color.lighter_grey())
         return await ctx.send(embed=embed)
 
-    win = False
-    if (shifumi_player == 'pierre' and shifumi_bot == 'ciseaux') or (
-            shifumi_player == 'ciseaux' and shifumi_bot == 'feuille') or (
-            shifumi_player == 'feuille' and shifumi_bot == 'pierre'):
-        win = True
-
-    if win:
+    if result in [-1, 2]:
         embed = discord.Embed(title="", description="I play '" + shifumi_bot + "'\nYou win!",
                               color=discord.Color.green())
         return await ctx.send(embed=embed)
